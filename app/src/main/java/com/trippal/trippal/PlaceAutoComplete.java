4d77405c52b1e91ce4596daf652def278f3ea178
c1rabbit <mail@calvinthanh.com>
2016-05-16 01:29:37 -0700

    auto complete for places

---
+++ app/src/main/java/com/trippal/trippal/PlaceAutoComplete.java
@@ -0,0 +1,56 @@
+package com.trippal.trippal;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.trippal.trippal.R;
+
+
+public class PlaceAutoComplete extends Fragment {
+
+    public final String TAG = "AutoComplete Frag";
+
+    public PlaceAutoComplete() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getActivity().getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
+        return inflater.inflate(R.layout.fragment_place_auto_complete, container, false);
+    }
+
+}
