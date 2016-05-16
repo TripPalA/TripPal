f1fe18c0e2905658090994d1d8aa4e4e3c908a48
c1rabbit <mail@calvinthanh.com>
2016-05-16 01:24:38 -0700

    changed back to original code

--- app/src/main/java/com/trippal/trippal/NewTripFragment.java
+++ app/src/main/java/com/trippal/trippal/NewTripFragment.java
@@ -3,15 +3,9 @@ package com.trippal.trippal;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 /**
  * Created by layla on 5/8/2016.
@@ -19,34 +13,11 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 public class NewTripFragment extends Fragment {
 
     View myView;
-    public final String TAG = "New Trip Fragment";
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.new_trip_layout, container, false);
-
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        try {
-            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-                @Override
-                public void onPlaceSelected(Place place) {
-                    // TODO: Get info about the selected place.
-                    Log.i(TAG, "Place: " + place.getName());
-                }
-
-                @Override
-                public void onError(Status status) {
-                    // TODO: Handle the error.
-                    Log.i(TAG, "An error occurred: " + status);
-                }
-            });
-        } catch (Exception e) {
-            Log.e(TAG, e.toString());
-        }
         return myView;
     }
 }
