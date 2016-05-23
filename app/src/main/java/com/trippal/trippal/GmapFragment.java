c6e6edd96f661f9fe8fef3170da38501dd20de7e
layla <laylaxh@gmail.com>
2016-05-22 20:51:29 -0700

    Successfully added GoogleMaps to NewTripFragment

---
+++ app/src/main/java/com/trippal/trippal/GmapFragment.java
@@ -0,0 +1,36 @@
+package com.trippal.trippal;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by layla on 5/22/2016.
+ */
+public class GmapFragment extends Fragment implements OnMapReadyCallback {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
