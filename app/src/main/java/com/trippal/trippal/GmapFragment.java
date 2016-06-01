1e19d6f45772e91eff8eecb84eb4ab8c78798b30
sam kim <seungkim11@gmail.com>
2016-06-01 15:46:33 -0700

    getchildFragmentManager working

--- app/src/main/java/com/trippal/trippal/GmapFragment.java
+++ app/src/main/java/com/trippal/trippal/GmapFragment.java
@@ -1,22 +1,56 @@
 package com.trippal.trippal;
 
+import android.annotation.TargetApi;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by layla on 5/22/2016.
  */
 public class GmapFragment extends Fragment {
 
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    GoogleMap mMap;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container,false);
+        View view = null;
+
+        if (servicesOK()){
+            Toast.makeText(getActivity(), "Ready to Map", Toast.LENGTH_SHORT).show();
+            view = inflater.inflate(R.layout.fragment_maps, container, false);
+        }else{
+            view = inflater.inflate(R.layout.content_main, container, false);
+        }
+
+        return view;
     }
+
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -25,5 +59,63 @@ public class GmapFragment extends Fragment {
 //        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
 //        fragment.getMapAsync(this);
     }
+
+    // checks if map service is connected
+    public boolean servicesOK() {
+
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, getActivity(), ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(getActivity(), "Can't connect to mapping service", Toast.LENGTH_SHORT);
+        }
+
+        return false;
+    }
+
+    private boolean initMap(){
+        if (mMap == null){
+
+        }
+    }
+
+//
+//    // geo location
+//    public void geoLocate(View v)throws IOException {
+//        hideSoftKeyboard(v);
+//
+//        TextView tv = (TextView) findViewById(R.id.map_editText_location);
+//        String searchString = tv.getText().toString();
+//
+//        Geocoder gc = new Geocoder(this);
+//
+//        List<Address> list = gc.getFromLocationName(searchString, 1);
+//
+//        if (list.size() > 0){
+//            Address address = list.get(0);
+//            String locality = address.getLocality();
+//            Toast.makeText(this, "Found: " + locality, Toast.LENGTH_SHORT).show();
+//
+//            double lat = address.getLatitude();
+//            double lng = address.getLongitude();
+//            gotoLocation(lat, lng, 15);
+//        }
+//    }
+//
+//    // hides softkey
+//    private void hideSoftKeyboard(View v) {
+//        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+//        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+//    }
+//
+//    // moves the camera location
+//    private void gotoLocation(double lat, double lng, int zoom) {
+//        LatLng latLng = new LatLng(lat, lng);
+//        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+//    }
 
 }
