e147afee41363626ade7b0ceb647bf70783ba423
c1rabbit <mail@calvinthanh.com>
2016-05-31 17:28:23 -0700

    - able to display all custom place info in list view with SimpleAdapter - able to clear all poi from db

--- app/src/main/java/com/trippal/trippal/MyPlace.java
+++ app/src/main/java/com/trippal/trippal/MyPlace.java
@@ -22,36 +22,44 @@ public class MyPlace {
     private final Context mContext;
 
     public MyPlace(Context context) {
+
         mContext = context;
+        init();
+    }
+
+    public void init() {
         TripDbHelper dbHelper = new TripDbHelper(this.mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         //db.execSQL("Drop table if exists saved_places");
 
-        Cursor c = db.rawQuery("select * from " + TripContract.PlaceEntry.TABLE_NAME, null);
+        try {
+            Cursor c = db.rawQuery("select * from " + TripContract.PlaceEntry.TABLE_NAME, null);
 
+            if (c.getCount() == 0) {
+                //set sample value if table is empty
+                ContentValues placeValue = new ContentValues();
+                placeValue.put(TripContract.PlaceEntry.COLUMN_PLACE_NAME, "Cal State LA");
+                placeValue.put(TripContract.PlaceEntry.COLUMN_PLACE_ADDRESS, "5151 State University Dr., Los Angeles, CA 90032");
+                placeValue.put(TripContract.PlaceEntry.COLUMN_GOOGLE_PLACE_ID, "ChIJLSHERe3PwoAR_jua-uyLzSA");
+                // placeValue.put(TripContract.PlaceEntry.COLUMN_COORD_LAT, "0");
+                //placeValue.put(TripContract.PlaceEntry.COLUMN_COORD_LONG, "0");
+                placeValue.put(TripContract.PlaceEntry.COLUMN_DATE, 1L);
+                Log.e("SAVE PLACE: ", "new table created");
+                long locationRowId;
+                locationRowId = db.insert(TripContract.PlaceEntry.TABLE_NAME, null, placeValue);
+                if (locationRowId < 0) {
+                    Log.e("SAVE PLACE: ", "could not add place into db.");
+                } else {
+                    Log.i("SAVE PLACE: ", "inserted in row id " + locationRowId);
+                }
 
-        if (c.getCount() < 0) {
+            }
+        } catch (Exception e) {
             //create table if does not exist
             dbHelper.onCreate(db);
-        }
-        if (c.getCount() == 0) {
-            //set sample value if table is empty
-            ContentValues placeValue = new ContentValues();
-            placeValue.put(TripContract.PlaceEntry.COLUMN_PLACE_NAME, "Cal State LA");
-            placeValue.put(TripContract.PlaceEntry.COLUMN_PLACE_ADDRESS, "5151 State University Dr., Los Angeles, CA 90032");
-            placeValue.put(TripContract.PlaceEntry.COLUMN_GOOGLE_PLACE_ID, "ChIJLSHERe3PwoAR_jua-uyLzSA");
-            // placeValue.put(TripContract.PlaceEntry.COLUMN_COORD_LAT, "0");
-            //placeValue.put(TripContract.PlaceEntry.COLUMN_COORD_LONG, "0");
-            placeValue.put(TripContract.PlaceEntry.COLUMN_DATE, 1L);
-            Log.e("SAVE PLACE: ", "new table created");
-            long locationRowId;
-            locationRowId = db.insert(TripContract.PlaceEntry.TABLE_NAME, null, placeValue);
-            if (locationRowId < 0) {
-                Log.e("SAVE PLACE: ", "could not add place into db.");
-            } else {
-                Log.i("SAVE PLACE: ", "inserted in row id " + locationRowId);
-            }
 
+            Log.i("", "Table does not exist. New table created.");
+            init();
         }
 
         db.close();
@@ -61,7 +69,6 @@ public class MyPlace {
 
         TripDbHelper dbHelper = new TripDbHelper(this.mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-
 
         ContentValues placeValue = new ContentValues();
         placeValue.put(TripContract.PlaceEntry.COLUMN_PLACE_NAME, place.getName().toString());
@@ -145,8 +152,14 @@ public class MyPlace {
 
         } while (c.moveToNext());
 
-
         return list;
+    }
 
+    public void clearPlaces() {
+        TripDbHelper dbHelper = new TripDbHelper(this.mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.execSQL("DROP TABLE IF EXISTS " + TripContract.PlaceEntry.TABLE_NAME);
+        init();
     }
 }
--- app/src/main/java/com/trippal/trippal/SavedPOIFragment.java
+++ app/src/main/java/com/trippal/trippal/SavedPOIFragment.java
@@ -1,26 +1,21 @@
 package com.trippal.trippal;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-import com.trippal.trippal.data.TripContract;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by layla on 5/8/2016.
@@ -52,13 +47,18 @@ public class SavedPOIFragment extends Fragment {
 
         listView = (ListView) myView.findViewById(R.id.savedPlacesListView);
         adapter =
-                new ArrayAdapter<MyPlace.SavedPlace>(getActivity(), R.layout.list_item, R.id.item_text, new ArrayList<MyPlace.SavedPlace>());
-        List<MyPlace.SavedPlace> placeList = myPlaces.getPlaces();
+                new ArrayAdapter<MyPlace.SavedPlace>(getActivity(), R.layout.list_item, R.id.item_name, new ArrayList<MyPlace.SavedPlace>());
+        List<Map<String, Object>> placeList = new ArrayList<Map<String, Object>>();
 
-        for (MyPlace.SavedPlace place : placeList) {
-            adapter.add(place);
+        for (MyPlace.SavedPlace place : myPlaces.getPlaces()) {
+            Map<String, Object> p = new HashMap<String, Object>();
+            p.put("name", place.getName());
+            p.put("gid", place.getGoogleId());
+            p.put("address", place.getAddress());
+            placeList.add(p);
         }
-        listView.setAdapter(adapter);
+
+        listView.setAdapter(new SimpleAdapter(getActivity(), placeList, R.layout.list_item, new String[]{"name", "address"}, new int[]{R.id.item_name, R.id.item_address}));
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -66,19 +66,21 @@ public class SavedPOIFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                final MyPlace.SavedPlace place = (MyPlace.SavedPlace) listView.getItemAtPosition(position);
-                Toast.makeText(getActivity(), place.getGoogleId().toString(), Toast.LENGTH_LONG).show();
+                Map<String, Object> place = (Map<String, Object>) listView.getItemAtPosition(position);
+                //TODO: open map for POI using google id
+                Toast.makeText(getActivity(), place.get("gid").toString(), Toast.LENGTH_LONG).show();
             }
         });
-        /*listView.setOnClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String placeName = listView.getItem(position).toString();
-                Toast.makeText(getActivity(), placeName, Toast.LENGTH_LONG).show();
-            }
 
-        });*/
-        //TODO: handle onclick event to open map, delete place
+        Button button = (Button) myView.findViewById(R.id.clear_all_poi);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myPlaces.clearPlaces();
+
+            }
+        });
+
 
         return myView;
     }
