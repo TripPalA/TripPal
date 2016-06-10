5ed9ce107afc85d504a046e1f76a484fb010a3c4
layla <laylaxh@gmail.com>
2016-06-09 19:28:13 -0700

    Code to display duration of trip. JSON parsing is incomplete - doesn't properly fetch yet

---
+++ app/src/main/java/com/trippal/trippal/FetchDurationTask.java
@@ -0,0 +1,163 @@
+package com.trippal.trippal;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.location.places.Place;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Layla on 6/9/2016.
+ */
+
+ /* get total distance and duration of trip
+         * URL contains JSON w/distance and duration given origin and destination
+         * https://maps.googleapis.com/maps/api/directions/json?origin=-20.291825,57.448668&destination=-20.179724,57.613463&key=AIzaSyDDbYFy3AmllmFdsh3Gy_-4nQcXsVQW040
+         */
+public class FetchDurationTask extends AsyncTask<String, Void, List<Place>> {
+
+    public interface AsyncResponse {
+        void processFinish(List<Place> markers);
+    }
+
+    public AsyncResponse delegate = null;
+
+    private final String LOG_TAG = FetchPlaceTask.class.getSimpleName();
+    private Activity activity;
+    private GoogleMap mMap;
+
+
+    public FetchDurationTask(Activity activity, GoogleMap mMap, AsyncResponse delegate) {
+        this.activity = activity;
+        this.mMap = mMap;
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected List<Place> doInBackground(String... params) {
+
+        if (params.length == 0) {
+            return null;
+        }
+
+        String origin = params[0];
+        String destination = params[1];
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String durationJsonStr = null;
+
+        try {
+
+            // Construct the URL for the query
+            final String FORMAT = "json";
+            final String BASE_URL = "https://maps.googleapis.com/maps/api/directions/" + FORMAT + "?";
+            final String ORIGIN_PARAM = "origin";
+            final String DEST_PARAM = "destination";
+            final String API_PARAM = "key";
+
+            // Build URI using constructions
+            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                    .appendQueryParameter(ORIGIN_PARAM, origin)
+                    .appendQueryParameter(DEST_PARAM, destination)
+                    .appendQueryParameter(API_PARAM, Utility.getApiKey(activity))
+                    .build();
+
+            //TODO: I want this: "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + dest + "&key=AIzaSyDDbYFy3AmllmFdsh3Gy_-4nQcXsVQW040";
+            // Back to a (properly constructed) URL
+            URL url = new URL(builtUri.toString());
+            Log.v(LOG_TAG, "URL:) : " + url);
+
+            // Create the request and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do
+                durationJsonStr = null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                durationJsonStr = null;
+            }
+
+            durationJsonStr = buffer.toString();
+            Log.v(LOG_TAG, "JSON: " + durationJsonStr);
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "ERROR", e);
+            e.printStackTrace();
+        }
+
+        try {
+            return parseJsonPlace(durationJsonStr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private List<Place> parseJsonPlace(String durationJsonStr) throws JSONException {
+
+        // Name of the JSON object that needs to be extracted
+        final String ROUTES = "routes";
+        final String LEGS = "legs";
+        final String DURATION = "duration";
+
+        JSONObject durationJson = new JSONObject(durationJsonStr);
+        JSONArray durationArray = durationJson.getJSONArray(ROUTES);
+
+        List<Place> list = new ArrayList<Place>();
+        for (int i = 0; i < durationArray.length(); i++) {
+            String totalDuration;
+            JSONObject o = durationArray.getJSONObject(i);
+            totalDuration = o.getString(DURATION);
+            //TODO: Need to finish logic in this method so it can properly go through the hierarchy and extract duration data
+        }
+
+        for (Place p : list) {
+            Log.v(LOG_TAG, "Duration: " + p);
+        }
+        return list;
+    }
+
+    @Override
+    protected void onPostExecute(List<Place> places) {
+        super.onPostExecute(places);
+        Log.v(LOG_TAG, "PLACES FOUND: " + String.valueOf(places.size()));
+        delegate.processFinish(places);
+    }
+}
+
--- app/src/main/java/com/trippal/trippal/GmapFragment.java
+++ app/src/main/java/com/trippal/trippal/GmapFragment.java
@@ -49,8 +49,12 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
